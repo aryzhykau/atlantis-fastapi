db5d79742fa0c31deead6c988de30bb65eef8b39
Andrei Ryzhykau <andrei.ryzhykau@outstaff.exness.com>
2025-03-18 11:52:01 +0100

    Add Alembic setup and update database schema
    This commit introduces Alembic for database migrations, including initial schema creation, modifying constraints, and renaming an enum type. It sets up necessary configurations, migration scripts, and database tables to support the application.

---
+++ alembic/versions/9061d3e64b6f_add_admin.py
@@ -0,0 +1,42 @@
+"""add_admin
+
+Revision ID: 9061d3e64b6f
+Revises: 9eb9360ae5ec
+Create Date: 2025-03-08 18:29:45.770554
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = '9061d3e64b6f'
+down_revision: Union[str, None] = '9eb9360ae5ec'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.execute(
+        sa.text(
+            """
+            INSERT INTO users (email, first_name, last_name, phone, role, google_authenticated)
+            VALUES (:email, :first_name, :last_name, :phone, :role, :google_authenticated)
+            """
+        ),
+        {
+            "email": "rorychan0697@gmail.com",
+            "first_name": "Андрей",
+            "last_name": "Рыжиков",
+            "phone": "+421940597865",
+            "role": "admin",  # Убедись, что значение соответствует тому, что хранится в БД
+            "google_authenticated": True,
+        }
+    )
+
+def downgrade() -> None:
+    op.execute(
+        sa.text("DELETE FROM users WHERE email = :email"),
+        {"email": "rorychan0697@gmail.com"}
+    )
+
