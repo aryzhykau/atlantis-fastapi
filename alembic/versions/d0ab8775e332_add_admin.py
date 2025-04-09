9a8c5af1d2a8eef0b49d8ed5048ffb5d75ec9bca
Andrei Ryzhykau <andrei.ryzhykau@outstaff.exness.com>
2025-04-09 23:42:25 +0200

    Add Docker setup and enhance subscription model
    Added `docker-compose.yml`, `Dockerfile.txt`, and `entrypoint.sh` to set up containers for frontend, backend, and database services. Improved the subscription model by introducing the `is_active` property, replacing the `active` index, and updated related references to ensure accurate subscription status tracking.

--- alembic/versions/d0ab8775e332_add_admin.py
+++ alembic/versions/d0ab8775e332_add_admin.py
@@ -7,9 +7,8 @@ Create Date: 2025-03-08 18:36:04.477838
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = 'd0ab8775e332'
@@ -30,6 +29,21 @@ def upgrade() -> None:
             first_name="Андрей",
             last_name="Рыжиков",
             phone="+421940597865",
+            role="ADMIN",  # Убедись, что это правильное значение в БД
+            google_authenticated=True
+        )
+    )
+    op.execute(
+        sa.text(
+            """
+            INSERT INTO users (email, first_name, last_name, phone, role, google_authenticated)
+            VALUES (:email, :first_name, :last_name, :phone, :role, :google_authenticated)
+            """
+        ).bindparams(
+            email="stadnykyuliia78@gmail.com",
+            first_name="Юлия",
+            last_name="Стадние",
+            phone="+421940597866",
             role="ADMIN",  # Убедись, что это правильное значение в БД
             google_authenticated=True
         )
